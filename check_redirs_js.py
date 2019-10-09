5609c7f77bc3ab4db32a444b16e919819b4a107b
George Mann <george.m.mann@gmail.com>
2019-10-09 10:24:20 -0400

    added john's code

--- check_redirs_js.py
+++ check_redirs_js.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
 import requests
 import csv
 import os
@@ -8,68 +9,52 @@ url_list_file = './urllist'
 output_file = './url_status.txt'
 output_file_bad = './url_responds_to_http.txt'
 port_test = '80'
-
 def isOpen(ip,port):
    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    try:
         s.connect((ip, int(port)))
         s.shutdown(2)
-        # http_open = 'True'
-        # # print(http_open)
+        http_open = 'True'
+        # print(http_open)
         return True
    except:
-        # http_open = 'False'
+        http_open = 'False'
         return False
         # print(http_open)
-
-
-
 def cleanup_old_file():
     if os.path.exists(output_file):
         os.remove(output_file)
-        print(output_file + "has been Removed!")
+        print(output_file + " has been Removed!")
     else:
         print('output file did not exist, continuing')
     if os.path.exists(output_file_bad):
         os.remove(output_file_bad)
-        print(output_file_bad + "has been Removed!")
+        print(output_file_bad + " has been Removed!")
     else:
         print('Http responding report output file did not exist, continuing')
 cleanup_old_file()
-
-
-
-
 def response_code_output():
     write_output_file.writelines('Site Name      : ' + url + ' \n' )
     write_output_file.writelines('Site Address   : ' + url_ip + ' \n' )
     write_output_file.writelines('Response Code  : ' + status_string + ' \n' )
-
 def response_code_output_bad():
     write_bad_output_file.writelines('Site Name      : ' + url + ' \n' )
     write_bad_output_file.writelines('Site Address   : ' + url_ip + ' \n' )
     write_bad_output_file.writelines('Response Code  : ' + status_string + ' \n' )
-
-# def __get_ip_of_hostname__():
-#     try:
-#            url_ip = socket.gethostbyname(url_row[0])
-#        except socket.gaierror
-#            pass
-#        except:
-#            print ("Unexpected error:", sys.exc_info()[0])
-#            #return ip
-
-
 with open(url_list_file, "r") as url_list:
     url_list_file_read = csv.reader(url_list)
     url_list.readline()
     for url_row in url_list_file_read:
         # url_ip = os.system("nslookup " + str(url_list_file_read))
         # print(url_row[0])
-        # __get_ip_of_hostname__
-        url_ip = socket.gethostbyname(url_row[0])
-        if not(url_ip):
-            print(url + 'cannot be resolved, moving on....')
+        
+        try:
+            url_ip = socket.gethostbyname(url_row[0])
+        except socket.gaierror:
+            pass
+        except:
+            print ("Unexpected error:", sys.exc_info()[0])
+            print(url_ip)
         port_is_open = isOpen(url_ip, port_test)
         if port_is_open:
             # print(url_ip)
@@ -93,7 +78,6 @@ with open(url_list_file, "r") as url_list:
                 response_code_output_bad()
                 write_bad_output_file.writelines('Result         : This responds to HTTP and is BAD!! \n' )
                 write_bad_output_file.writelines('\n' )
-
             elif r.status_code in (302, 301) :
                 response_code_output()
                 # write_output_file.writelines('Site          : ' + url + ' \n' )
@@ -107,5 +91,5 @@ with open(url_list_file, "r") as url_list:
                 # write_output_file.writelines('Response Code : ' + status_string + ' \n' )
                 write_output_file.writelines('Result         : This is likely ok and redirects probably \n' )
                 write_output_file.writelines('\n' )
-        write_output_file.writelines('Report Complete \n')
-        write_output_file.close
+        #write_output_file.writelines('Report Complete \n')
+        #write_output_file.close()
