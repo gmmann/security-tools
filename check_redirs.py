614de0a5f758513fdec904b0e255cf9e478b69d6
George Mann <george.m.mann@gmail.com>
2019-10-08 20:30:04 -0400

    .

--- check_redirs.py
+++ check_redirs.py
@@ -3,7 +3,6 @@ import requests
 import csv
 import os
 import sys
-import nmap 
 import socket
 url_list_file = './urllist'
 output_file = './url_status.txt'
@@ -16,18 +15,13 @@ def isOpen(ip,port):
         s.connect((ip, int(port)))
         s.shutdown(2)
         http_open = 'True'
-        print(http_open)
+        # print(http_open)
         return True
    except:
         http_open = 'False'
         return False
-        print(http_open)
+        # print(http_open)
 
-# scan = nmap.port_scan()
-# scan.scan('192.168.69.9', '80')
-# print(scan.comm)
-
-# port_scan = scan.scan('192.168.69.9', '80')
 
 
 def cleanup_old_file():
@@ -54,7 +48,7 @@ def response_code_output():
 def response_code_output_bad():
     write_bad_output_file.writelines('Site Name      : ' + url + ' \n' )
     write_bad_output_file.writelines('Site Address   : ' + url_ip + ' \n' )
-    write_bad_output_file.writelines('Response Code  : ' + status_string + ' \n' )    
+    write_bad_output_file.writelines('Response Code  : ' + status_string + ' \n' )
 
 
 
@@ -66,43 +60,44 @@ with open(url_list_file, "r") as url_list:
         # url_ip = os.system("nslookup " + str(url_list_file_read))
         # print(url_row[0])
         url_ip = socket.gethostbyname(url_row[0])
-        isOpen(url_ip, port_test)
-        print(url_ip)
-        print(port_test)
-        if http_open():
-            print (url_ip)
-            print('HEY')
-        url = 'http://' + url_row[0]
-        # print(url)
-        r = requests.get(url, allow_redirects=False)
-        # print(r.url)
-        # print(r.history)
-        # print(r.status_code)
-        status_string = str(r.status_code)
-        write_output_file = open(output_file,'a+')
-        write_bad_output_file = open(output_file_bad,'a+')
-        if r.status_code == 200:
-            response_code_output()    
-            # write_output_file.writelines ('Site          : ' + url + ' \n' )
-            # write_output_file.writelines('Response Code : ' + status_string + ' \n' )
-            write_output_file.writelines('Result         : This responds to HTTP and is BAD!! \n' )     
-            write_output_file.writelines('\n' ) 
-            response_code_output_bad()
-            write_bad_output_file.writelines('Result         : This responds to HTTP and is BAD!! \n' )     
-            write_bad_output_file.writelines('\n' ) 
+        if not(url_ip):
+            print(url + 'cannot be resolved, moving on....')
+        port_is_open = isOpen(url_ip, port_test)
+        if port_is_open:
+            # print(url_ip)
+            # print(port_is_open)
+            # print(http_open)
+            url = 'http://' + url_row[0]
+            # print(url)
+            r = requests.get(url, allow_redirects=False)
+            # print(r.url)
+            # print(r.history)
+            # print(r.status_code)
+            status_string = str(r.status_code)
+            write_output_file = open(output_file,'a+')
+            write_bad_output_file = open(output_file_bad,'a+')
+            if r.status_code == 200:
+                response_code_output()
+                # write_output_file.writelines ('Site          : ' + url + ' \n' )
+                # write_output_file.writelines('Response Code : ' + status_string + ' \n' )
+                write_output_file.writelines('Result         : This responds to HTTP and is BAD!! \n' )
+                write_output_file.writelines('\n' )
+                response_code_output_bad()
+                write_bad_output_file.writelines('Result         : This responds to HTTP and is BAD!! \n' )
+                write_bad_output_file.writelines('\n' )
 
-        elif r.status_code in (302, 301) :
-            response_code_output()    
-            # write_output_file.writelines('Site          : ' + url + ' \n' )
-            # write_output_file.writelines('Response Code : ' + status_string + ' \n' )
-            write_output_file.writelines('Result         : This is ok and redirects \n' )     
-            write_output_file.writelines('\n' )             
-            # print('This is ok and redirects')
-        else:
-            response_code_output()
-            # write_output_file.writelines('Site          : ' + url + ' \n' )
-            # write_output_file.writelines('Response Code : ' + status_string + ' \n' )
-            write_output_file.writelines('Result         : This is likely ok and redirects probably \n' )     
-            write_output_file.writelines('\n' )             
-    write_output_file.writelines('Report Complete \n')
-    write_output_file.close
+            elif r.status_code in (302, 301) :
+                response_code_output()
+                # write_output_file.writelines('Site          : ' + url + ' \n' )
+                # write_output_file.writelines('Response Code : ' + status_string + ' \n' )
+                write_output_file.writelines('Result         : This is ok and redirects \n' )
+                write_output_file.writelines('\n' )
+                # print('This is ok and redirects')
+            else:
+                response_code_output()
+                # write_output_file.writelines('Site          : ' + url + ' \n' )
+                # write_output_file.writelines('Response Code : ' + status_string + ' \n' )
+                write_output_file.writelines('Result         : This is likely ok and redirects probably \n' )
+                write_output_file.writelines('\n' )
+        write_output_file.writelines('Report Complete \n')
+        write_output_file.close
